ea352eac371f9b6986c4daf5d7e0399ce3c373b3
chenE2000 <396909028@qq.com>
2022-07-24 13:30:11 +0800

    chore: 一些cleanup 安装antdesign库

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import Antd from 'ant-design-vue';
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import 'ant-design-vue/dist/antd.css';
+
+createApp(App).use(Antd).mount('#app')
