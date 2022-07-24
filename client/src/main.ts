120dfb41277d7bab440d92e7ee8ba2c847811e42
chenE2000 <396909028@qq.com>
2022-07-24 14:53:28 +0800

    feat: 用户管理和数据管理的初步搭建

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
+import { router } from "./router";
+
 import './style.css'
 import Antd from 'ant-design-vue';
 
 import App from './App.vue'
 import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(Antd).mount('#app')
+createApp(App)
+    .use(Antd)
+    .use(router)
+    .mount('#app')
